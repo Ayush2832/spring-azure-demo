5e444c8eb847def571f2875b30c671c1c329ae06
Ayush <ayush28.connect@gmail.com>
2024-03-13 22:26:49 +0530

    Update SpringAzureDemoApplication.java

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+		return "Congrats !dfghjk;lkjhgv kiuygfv mjhgvm. !";
 	}
 
 	public static void main(String[] args) {
